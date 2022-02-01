4ac0e55673f86e1af0bb0d5dd419c1f764d8837f
Denis <jujojq@gmail.com>
2022-02-01 15:11:10 +0300

    added delete

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { createStore } from "redux";
+import { taskReducer } from "./taskReducer";
+
+const initialState = [
+  { id: 1, title: "Task1", complited: false },
+  { id: 2, title: "Task2", complited: false },
+];
+
+export function initiateStore() {
+  return createStore(taskReducer, initialState);
+}
